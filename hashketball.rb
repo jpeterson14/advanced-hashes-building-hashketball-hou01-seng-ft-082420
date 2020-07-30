c4b3645c9b516a5f0a0e4a827b5eef99c60e0d34
Jake Peterson <jpeterson0514@gmail.com>
2020-07-30 22:24:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,112 @@
-# Write your code here!
+def game_hash
+  {:home => {
+  :team_name => "Brooklyn Nets", 
+  :colors => ["Black", "White"],
+  :players => [{
+    :player_name => "",
+    :number => "",
+    :shoe => "",
+    :points => "",
+    :rebounds => "",
+    :assists => "",
+    :steals => "" ,
+    :blocks => "",
+    :slam_dunks => ""
+    }, {
+    :player_name => "",
+    :number => "",
+    :shoe => "",
+    :points => "",
+    :rebounds => "",
+    :assists => "",
+    :steals => "" ,
+    :blocks => "",
+    :slam_dunks => ""
+    }, {
+    :player_name => "",
+    :number => "",
+    :shoe => "",
+    :points => "",
+    :rebounds => "",
+    :assists => "",
+    :steals => "" ,
+    :blocks => "",
+    :slam_dunks => ""
+    }, {
+    :player_name => "",
+    :number => "",
+    :shoe => "",
+    :points => "",
+    :rebounds => "",
+    :assists => "",
+    :steals => "" ,
+    :blocks => "",
+    :slam_dunks => ""
+    }, {
+    :player_name => "",
+    :number => "",
+    :shoe => "",
+    :points => "",
+    :rebounds => "",
+    :assists => "",
+    :steals => "" ,
+    :blocks => "",
+    :slam_dunks => ""
+    }]
+  },
+  :away => {
+  :team_name => "team name", 
+  :colors => ["colors"],
+  :players => [{
+    :player_name => "",
+    :number => "",
+    :shoe => "",
+    :points => "",
+    :rebounds => "",
+    :assists => "",
+    :steals => "" ,
+    :blocks => "",
+    :slam_dunks => ""
+    }, {
+    :player_name => "",
+    :number => "",
+    :shoe => "",
+    :points => "",
+    :rebounds => "",
+    :assists => "",
+    :steals => "" ,
+    :blocks => "",
+    :slam_dunks => ""
+    }, {
+    :player_name => "",
+    :number => "",
+    :shoe => "",
+    :points => "",
+    :rebounds => "",
+    :assists => "",
+    :steals => "" ,
+    :blocks => "",
+    :slam_dunks => ""
+    }, {
+    :player_name => "",
+    :number => "",
+    :shoe => "",
+    :points => "",
+    :rebounds => "",
+    :assists => "",
+    :steals => "" ,
+    :blocks => "",
+    :slam_dunks => ""
+    }, {
+    :player_name => "",
+    :number => "",
+    :shoe => "",
+    :points => "",
+    :rebounds => "",
+    :assists => "",
+    :steals => "" ,
+    :blocks => "",
+    :slam_dunks => ""
+    }]
+  }}
+end
